9643016d3e38b3d47ebe877d1be7429f03db091f
Ultra-Man <techfixer@live.com>
2023-03-09 04:21:23 +0000

    Update docker-compose.yml & app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -38,9 +38,10 @@ tracer = trace.get_tracer(__name__)
 # X-RAY ---------------------------
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
 
+# X-RAY ---------------------------
 app = Flask(__name__)
+XRayMiddleware(app, xray_recorder)
 
 # HoneyComb ----------------------------
 # Initialize automatic instrumentation with Flask
