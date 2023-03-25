73c339cd12a7d1e56b0459ef5a1e90be4cb03fa4
Ultra-Man <59581412+ultraman-labs@users.noreply.github.com>
2023-03-25 14:47:07 -0700

    Update seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,13 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Tony Quintanilla', 'tonyquintanilla' ,'MOCK'),
-  ('Tony David', 'David' ,'MOCK');
+  ('Tony Quintanilla', '<email>', 'tonyquintanilla' ,'MOCK'),
+  ('Cassidy Tuck', '<email>', 'cassidytuck' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'tonyquintanilla' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
