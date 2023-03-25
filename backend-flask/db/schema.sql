81241bdbe2c67baef6561b18dd762a5c7c120173
Ultra-Man <techfixer@live.com>
2023-03-25 21:08:38 +0000

    Update schema.sql file

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -5,16 +5,16 @@ DROP TABLE IF EXISTS public.activities;
 
 CREATE TABLE public.users (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  display_name text,
-  handle text,
-  email text,
-  cognito_user_id text,
+  display_name text NOT NULL,
+  handle text NOT NULL,
+  email text NOT NULL,
+  cognito_user_id text NOT NULL,
   created_at TIMESTAMP default current_timestamp NOT NULL
 );
 
 CREATE TABLE public.activities (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  user_uuid UUID,
+  user_uuid UUID NOT NULL,
   message text NOT NULL,
   replies_count integer DEFAULT 0,
   reposts_count integer DEFAULT 0,
