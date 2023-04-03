d3adc3563d446b24e2999aec81ee3bd075a05660
Ultra-Man <techfixer@live.com>
2023-04-03 21:15:45 +0000

    Added notifications_activities.py modified app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
-import sys
+# import sys
 
 #from services.users_short import *
 from services.home_activities import *
