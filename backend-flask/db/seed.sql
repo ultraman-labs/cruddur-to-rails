eb6b860ba0ca946d0ae51dbe886ef7ae040516c3
Ultra-Man <techfixer@live.com>
2023-04-18 07:09:58 +0000

    Seeding Londo record

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,7 @@ INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Tony Quintanilla', 'nodaltech@outlook.com', 'Ultra-Man' ,'50790df1-cc21-4ea3-8f43-a064d4cf07b2'),
   ('Cassidy Tuck', 'techfixer@live.com', 'Ultra-Girl' ,'53b535ba-864b-447b-a1bc-026a4ddd1f0e');
-  ('Londo Mollari', 'lmollari@centari.com', 'londo' ,'MOCK');
+  ('Londo Mollari', 'lmollari@centari.com', 'londo' ,'22z535ba-3358e-447b-a1bc-026a4ddd1f0e');
 
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
