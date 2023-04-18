2ee0eedbd9979351f943fc5bd26c86c9274239ea
Ultra-Man <techfixer@live.com>
2023-04-18 06:15:20 +0000

    Implementing @handle

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,6 +3,8 @@ INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Tony Quintanilla', 'nodaltech@outlook.com', 'Ultra-Man' ,'50790df1-cc21-4ea3-8f43-a064d4cf07b2'),
   ('Cassidy Tuck', 'techfixer@live.com', 'Ultra-Girl' ,'53b535ba-864b-447b-a1bc-026a4ddd1f0e');
+  ('Londo Mollari', 'lmollari@centari.com', 'londo' ,'MOCK');
+
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
