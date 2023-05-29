cf97e3a55a89443af3f813f003ffb7fcf0b8a1fd
Ultra-Man <techfixer@live.com>
2023-05-29 03:52:36 +0000

    Debugging CheckAuth setUser not a funciton issue

--- frontend-react-js/src/lib/CheckAuth.js
+++ frontend-react-js/src/lib/CheckAuth.js
@@ -1,6 +1,4 @@
 import { Auth } from 'aws-amplify';
-import { resolvePath } from 'react-router-dom';
-
 export async function getAccessToken(){
   Auth.currentSession()
   .then((cognito_user_session) => {
@@ -9,7 +7,6 @@ export async function getAccessToken(){
   })
   .catch((err) => console.log(err));
 }
-
 export async function checkAuth(setUser){
   Auth.currentAuthenticatedUser({
     // Optional, By default is false. 
@@ -18,14 +15,13 @@ export async function checkAuth(setUser){
     bypassCache: false 
   })
   .then((cognito_user) => {
-    console.log('cognito_user',cognito_user);
     setUser({
+      cognito_user_uuid: cognito_user.attributes.sub,
       display_name: cognito_user.attributes.name,
       handle: cognito_user.attributes.preferred_username
     })
     return Auth.currentSession()
   }).then((cognito_user_session) => {
-      console.log('cognito_user_session',cognito_user_session);
       localStorage.setItem("access_token", cognito_user_session.accessToken.jwtToken)
   })
   .catch((err) => console.log(err));
