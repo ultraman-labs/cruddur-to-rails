eed3ed63f0580137719bfb722fe2625699c38f15
Ultra-Man <techfixer@live.com>
2023-06-03 05:20:22 +0000

    Changed path from absoulte lib/CheckAuth to relative ../lib/CheckAuth was able to build frontend docker image

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -6,7 +6,7 @@ import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
 import ReplyForm from '../components/ReplyForm';
-import {checkAuth, getAccessToken} from 'lib/CheckAuth';
+import {checkAuth, getAccessToken} from '../lib/CheckAuth';
 
 export default function HomeFeedPage() {
   const [activities, setActivities] = React.useState([]);
