32b989367061b93ea2188e1ba296180edcfced17
Ultra-Man <techfixer@live.com>
2023-07-11 22:28:17 +0000

    Script rewrites

--- bin/updates-to-ddb/retrieve_records_for_speciific_uuid_message_uuid.py
+++ bin/updates-to-ddb/retrieve_records_for_speciific_uuid_message_uuid.py
@@ -7,15 +7,16 @@ dynamodb = boto3.client('dynamodb')
 # Define table name
 table_name = 'cruddur-messages'
 
-# Prompt the user to enter the user_uuid
+# Prompt the user to enter the user_uuid and message_uuid
 user_uuid = input("Enter the user_uuid: ")
+message_uuid = input("Enter the message_uuid: ")
 
-# Query the table to retrieve the items with the matching user_uuid
+# Query the table to retrieve the items with the matching user_uuid and message_uuid
 response = dynamodb.scan(
     TableName=table_name,
-    FilterExpression='#uu = :user_uuid',
-    ExpressionAttributeNames={'#uu': 'user_uuid'},
-    ExpressionAttributeValues={':user_uuid': {'S': user_uuid}},
+    FilterExpression='#uu = :user_uuid AND #mu = :message_uuid',
+    ExpressionAttributeNames={'#uu': 'user_uuid', '#mu': 'message_uuid'},
+    ExpressionAttributeValues={':user_uuid': {'S': user_uuid}, ':message_uuid': {'S': message_uuid}},
     ProjectionExpression='pk, user_display_name, message, message_group_uuid, message_uuid, user_uuid, user_handle'
 )
 
@@ -23,7 +24,7 @@ response = dynamodb.scan(
 items = response['Items']
 
 if len(items) == 0:
-    print("No records with the specified user_uuid were found.")
+    print("No records with the specified user_uuid and message_uuid were found.")
 else:
     num_records = len(items)
     num_items = sum(len(item) for item in items)
--- bin/updates-to-ddb/update_records_by_user_uuid_message_uuid.py
+++ bin/updates-to-ddb/update_records_by_user_uuid_message_uuid.py
@@ -8,20 +8,21 @@ dynamodb = boto3.client('dynamodb')
 # Define table name
 table_name = 'cruddur-messages'
 
-# Prompt user for old_user_uuid, new_user_uuid, and number of records to update
+# Prompt user for old_user_uuid, new_user_uuid, message_uuid, and number of records to update
 old_user_uuid = input("Enter the old user_uuid: ")
 new_user_uuid = input("Enter the new user_uuid: ")
+message_uuid = input("Enter the message_uuid: ")
 num_records = int(input("Enter the number of records to update: "))
 
 # Define the user_handle
 user_handle = 'Ultra-Man'
 
-# Query the table to retrieve the specified number of items with the matching user_handle
+# Query the table to retrieve the specified number of items with the matching user_handle and message_uuid
 response = dynamodb.scan(
     TableName=table_name,
-    FilterExpression='#uh = :user_handle',
-    ExpressionAttributeNames={'#uh': 'user_handle'},
-    ExpressionAttributeValues={':user_handle': {'S': user_handle}},
+    FilterExpression='#uh = :user_handle AND #mu = :message_uuid',
+    ExpressionAttributeNames={'#uh': 'user_handle', '#mu': 'message_uuid'},
+    ExpressionAttributeValues={':user_handle': {'S': user_handle}, ':message_uuid': {'S': message_uuid}},
     ProjectionExpression='pk',
     Limit=num_records
 )
@@ -61,9 +62,9 @@ while items and updated_count < num_records:
     # Query the table again to check if there are more items
     response = dynamodb.scan(
         TableName=table_name,
-        FilterExpression='#uh = :user_handle',
-        ExpressionAttributeNames={'#uh': 'user_handle'},
-        ExpressionAttributeValues={':user_handle': {'S': user_handle}},
+        FilterExpression='#uh = :user_handle AND #mu = :message_uuid',
+        ExpressionAttributeNames={'#uh': 'user_handle', '#mu': 'message_uuid'},
+        ExpressionAttributeValues={':user_handle': {'S': user_handle}, ':message_uuid': {'S': message_uuid}},
         ProjectionExpression='pk',
         Limit=num_records - updated_count
     )
