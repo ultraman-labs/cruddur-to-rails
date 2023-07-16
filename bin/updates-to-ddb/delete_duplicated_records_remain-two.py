3f31c3bdca9de7a08fc771fa8b83d539c3952091
Ultra-Man <techfixer@live.com>
2023-07-16 07:59:42 +0000

    Deleting records script now working

--- bin/updates-to-ddb/delete_duplicated_records_remain-two.py
+++ bin/updates-to-ddb/delete_duplicated_records_remain-two.py
@@ -1,4 +1,5 @@
 import boto3
+from collections import defaultdict
 
 # Create DynamoDB client
 dynamodb = boto3.client('dynamodb')
@@ -16,7 +17,7 @@ response = dynamodb.scan(
     FilterExpression='#uu = :user_uuid AND #msg = :message',
     ExpressionAttributeNames={'#uu': 'user_uuid', '#msg': 'message'},
     ExpressionAttributeValues={':user_uuid': {'S': user_uuid}, ':message': {'S': message}},
-    ProjectionExpression='pk, sk, message'
+    ProjectionExpression='pk, sk'
 )
 
 # Retrieve the items associated with the user_uuid and message
@@ -27,44 +28,46 @@ if len(items) == 0:
 else:
     print(f"Number of records retrieved: {len(items)}")
 
-    # Create a dictionary to track duplicate messages
-    duplicates = {}
+    # Create a dictionary to track duplicate records
+    duplicates = defaultdict(list)
 
     for item in items:
+        pk = item.get('pk', {}).get('S')
         sk = item.get('sk', {}).get('S')
-        if sk:
-            if sk not in duplicates:
-                duplicates[sk] = {
-                    'count': 1,
-                    'pk': item.get('pk', {}).get('S'),
-                    'message': item.get('message', {}).get('S')
-                }
-            else:
-                duplicates[sk]['count'] += 1
+        if pk and sk:
+            duplicates[pk].append(sk)
 
-    if duplicates:
-        print("\nDuplicate records found:")
-        remaining_duplicates = 2
+    # Determine the number of duplicate records to delete
+    total_duplicates = 0
+    for pk, sk_list in duplicates.items():
+        if len(sk_list) > 2:
+            total_duplicates += len(sk_list) - 2
+
+    print(f"Number of duplicate records found: {total_duplicates}")
+
+    # Prompt the user to confirm deletion
+    proceed = input("Do you want to proceed with deleting duplicate records? (yes/no): ")
+
+    if proceed.lower() == 'yes':
         deleted_count = 0
-        for sk, data in duplicates.items():
-            count = data['count']
-            if count > remaining_duplicates:
-                deleted_count += 1
-                # Delete the extra duplicate records
-                response = dynamodb.delete_item(
-                    TableName=table_name,
-                    Key={
-                        'pk': {'S': data['pk']},
-                        'sk': {'S': sk}
-                    }
-                )
+
+        for pk, sk_list in duplicates.items():
+            if len(sk_list) > 2:
+                # Delete extra duplicate records
+                records_to_delete = sk_list[:-2]
+
+                # Delete the records in batches
+                while len(records_to_delete) > 0:
+                    batch = [{'DeleteRequest': {'Key': {'pk': {'S': pk}, 'sk': {'S': sk}}}} for sk in records_to_delete[:25]]
+                    response = dynamodb.batch_write_item(RequestItems={table_name: batch})
+                    deleted_count += len(response.get('UnprocessedItems', {}).get(table_name, []))
+                    records_to_delete = records_to_delete[25:]
 
         print(f"Number of duplicate records deleted: {deleted_count}")
-
     else:
-        print("\nNo duplicate records found.")
+        print("Deletion process aborted.")
 
-    # Query the table to retrieve the items with the matching user_uuid and message
+    # Query the table to retrieve the remaining items with the matching user_uuid and message
     response = dynamodb.scan(
         TableName=table_name,
         FilterExpression='#uu = :user_uuid AND #msg = :message',
@@ -73,7 +76,7 @@ else:
         ProjectionExpression='pk, message'
     )
 
-    # Retrieve the items associated with the user_uuid and message
+    # Retrieve the remaining items associated with the user_uuid and message
     remaining_items = response['Items']
     print(f"\nNumber of remaining records: {len(remaining_items)}")
     print("Value of 'message' attribute in remaining records:")
