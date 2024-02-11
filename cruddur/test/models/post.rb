91fcc0264d54c84b0063730249fd951445dcec50
Ultra-Man <techfixer@live.com>
2024-02-11 04:52:50 +0000

    Manually created the model files of users.rb and post.rb

---
+++ cruddur/test/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+    # Here you can add your model's associations, validations, and custom methods
+    # For example, if your posts belong to a user, you would add:
+    # belongs_to :user
+  end
+  
---
+++ cruddur/test/models/users.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+    # Here you can add your model's associations, validations, and custom methods
+  end
+  
