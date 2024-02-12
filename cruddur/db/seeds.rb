5ebbaf7928af53460b3b9de5490202ee6e2b0f5c
Ultra-Man <techfixer@live.com>
2024-02-12 21:39:58 +0000

    Troubleshooting seeds.rb not populating db

--- cruddur/db/seeds.rb
+++ cruddur/db/seeds.rb
@@ -1,10 +1,9 @@
 # Create a user
 user = User.find_or_create_by!(name: 'Ultra-Man', email: 'tony@ultra-labs.com') do |u|
-    u.password_digest = 'password' # Replace with a valid password digest
-    # Add other necessary user attributes here
-  end
-  
-  # Create posts associated with the created user
-  user.posts.find_or_create_by!(name: 'Que pasa vato!', description: 'Somos hermanos!')
-  # Add more posts as needed, ensuring to associate them with the user
-  
+  u.password = 'password' # Set a valid password
+  # Add other necessary user attributes here
+end
+
+# Create posts associated with the created user
+user.posts.find_or_create_by!(name: 'Que pasa vato!', description: 'Somos hermanos!')
+# Add more posts as needed, ensuring to associate them with the user
